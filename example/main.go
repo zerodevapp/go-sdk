5bd5ff23335b70edccd57e73831836d6fdc30c9e
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2025-10-30 00:15:01 +0530

    chore: update example command

--- example/main.go
+++ example/main.go
@@ -22,13 +22,13 @@ func logJSON(v interface{}) {
 func printUsage() {
 	fmt.Println("ZeroDev Go SDK Examples")
 	fmt.Println("\nUsage:")
-	fmt.Println("  go run . <command>")
+	fmt.Println("  go run example/main.go <args>")
 	fmt.Println("\nAvailable commands:")
-	fmt.Println("  4337    Run 4337 account example")
-	fmt.Println("  7702    Run 7702 account example (full flow with authorization)")
-	fmt.Println("  help       Show this help message")
+	fmt.Println("  -e  4337    Run 4337 account example")
+	fmt.Println("  -e  7702    Run 7702 account example (full flow with authorization)")
+	fmt.Println("  help              Show this help message")
 	fmt.Println("\nExamples:")
-	fmt.Println("  go run . 7702")
+	fmt.Println("  go run example/main.go -e 7702")
 	fmt.Println("\nMake sure to set up your .env file with ZERODEV_PROJECT_ID and USEROP_BUILDER_API_KEY")
 }
 
@@ -38,26 +38,27 @@ func main() {
 		log.Println("No .env file found, using system environment variables")
 	}
 
-	flag.Parse()
-	args := flag.Args()
+	example := flag.String("e", "7702", "The example to run: '4337' or '7702'")
 
-	if len(args) == 0 {
+	flag.Parse()
+
+	if *example == "" {
 		fmt.Println("Error: No command specified")
 		printUsage()
 		os.Exit(1)
 	}
 
-	command := args[0]
-
-	switch command {
+	switch *example {
 	case "7702":
-		run4337Example()
-	case "4337":
+		log.Println("Running 7702 example...")
 		run7702Example()
+	case "4337":
+		log.Println("Running 4337 example...")
+		run4337Example()
 	case "help", "-h", "--help":
 		printUsage()
 	default:
-		fmt.Printf("Error: Unknown command '%s'\n\n", command)
+		fmt.Printf("Error: Unknown command '%s'\n\n", *example)
 		printUsage()
 		os.Exit(1)
 	}
